21fd26c971347fecdf8bf1299739b91467524760
yusstyle <yusufhussaini0904@gmail.com>
2025-11-02 09:13:21 +0100

    Fix deployment

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,12 @@
-@'
-/** @type {import('tailwindcss').Config} */
+﻿/** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: 'class', // or 'media'
   theme: {
     extend: {},
   },
   plugins: [],
 }
-'@ | Set-Content -Path tailwind.config.js -Encoding utf8

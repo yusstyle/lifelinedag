8220b0b53b285391916911d8a2da05327b917f7a
yusstyle <yusufhussaini0904@gmail.com>
2025-11-02 09:43:28 +0100

    Deploy updated version

--- postcss.config.js
+++
@@ -1,6 +0,0 @@
-﻿export default {
-  plugins: {
-    '@tailwindcss/postcss': {},
-    autoprefixer: {},
-  },
-}
--- vite.config.js
+++
@@ -1,7 +0,0 @@
-﻿import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  base: "./",
-  plugins: [react()],
-});
